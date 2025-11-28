d71a2bbbb7a380106d7ff26f1edecc43e4ea7f15
StevanCorak <djuro.muric@gmail.com>
2025-11-28 09:41:39 +0100

    da

--- myapp/__init__.py
+++ myapp/__init__.py
@@ -7,20 +7,15 @@ def create_app():
     Stvara i konfigurira Flask aplikaciju.
     """
     app = Flask(__name__, instance_relative_config=True)
-    
-    # Postavljanje jednostavne konfiguracije. 
-    # U stvarnom sustavu, ovo bi bilo kompleksnije.
     app.config.from_mapping(
         SECRET_KEY='dev_secret_key_for_checkpoint1',
     )
 
-    # Stvaranje foldera za instance (nije striktno potrebno, ali je dobra praksa)
     try:
         os.makedirs(app.instance_path)
     except OSError:
         pass
 
-    # Registracija Blueprinta (skupine ruta) za korisničko sučelje
     app.register_blueprint(ui_bp)
 
     return app
